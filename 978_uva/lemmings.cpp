332cb7f1446fd48cae0a43765ffaccd6bb44d65d
Guilherme Martins <gui.amado29@gmail.com>
2018-10-09 16:03:00 +0100

    added lemmings problem

---
+++ 978_uva/lemmings.cpp
@@ -0,0 +1,23 @@
+
+#include<iostream> //std::cout
+#include<queue>    //std::priority_queue
+
+using namespace std;
+
+int main(){
+  int ntest; cin >> ntest;
+  while(ntest-- > 0){
+    //inicialization
+    int b, sg, sb;
+    cin >> b;
+    cin >> sg;
+    cin >> sb;
+    priority_queue<int> green_army;
+    priority_queue<int> blue_army;
+    
+    
+    
+
+  }
+  
+}
