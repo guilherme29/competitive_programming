3ebd984fdaa82e994814f77ec06462223e498942
Guilherme Martins <gui.amado29@gmail.com>
2018-10-19 16:42:04 +0100

    finished lemmings problem

--- 978_uva/lemmings.cpp
+++ 978_uva/lemmings.cpp
@@ -1,68 +1,69 @@
-
-#include<iostream> //std::cout
-#include<queue>    //std::priority_queue
-#include<list>     //std::list
+#include<iostream>
+#include<list>
+#include<queue>
 using namespace std;
+
 
 
 int main(){
   int ntest; cin >> ntest;
   while(ntest-- > 0){
-    int nbf, sg, sb;//number of battlefields and of green and blue lemmings respectively
-    cin >> nbf;
-    cin >> sg;
-    cin >> sb;
+    int nbfs; cin >> nbfs; //number of battlefields
+    int ngreen; cin >> ngreen; //number of green lemmings
+    int nblue; cin >> nblue; //number of blue lemmings
     priority_queue<int> green_army;
     priority_queue<int> blue_army;
     //to prevent soldiers from fighting again, we store them on a list temporarly
     list<int> green_done;
     list<int> blue_done;
-
     //reading each soldier stats
     int k;
-    for(int i=0;i<sg;i++){
+    for(int i=0;i<ngreen;i++){
       cin >> k;
       green_army.push(k);
     }
-    for(int i=0;i<sb;i++){
+    for(int i=0;i<nblue;i++){
       cin >> k;
       blue_army.push(k);
     }
-    
 
-    while(!green_army.empty() && !blue_army.empty()){
-      while(green_army.size()>0 && blue_army.size()>0){
-	int green = green_army.top();
-	green_army.pop();
-	int blue = blue_army.top();
-	blue_army.pop();
-	
-	if(blue > green){
-	  int victor = blue - green;
-	  if(victor != 0){
+    while(green_army.size()>0 && blue_army.size()>0){
+      for(int i=0;i<nbfs;i++){//for each battlefield
+	if(green_army.size()>0 && blue_army.size()>0){
+	  int green = green_army.top();
+	  green_army.pop();
+	  int blue = blue_army.top();
+	  blue_army.pop();
+
+	  int victor = 0;
+	  if(blue > green){ //blue wins
+	    victor = blue - green;
 	    blue_done.push_back(victor);
 	  }
-	}
-        else if(blue < green){
-	  int victor = green - blue;
-	  if(victor != 0){
+	  else if(blue < green){ //green wins
+	    victor = green - blue;
 	    green_done.push_back(victor);
 	  }
+	  
 	}
+	else break;
       }
-      //putting the soldiers who fought back in the priority queue
+      
+      //push the battle hardened lemmings back into the ranks
+      int elem = 0;
+      while(blue_done.size() > 0){ 
+	elem = blue_done.front();
+	blue_done.pop_front();
+	blue_army.push(elem);
+      }
       while(green_done.size() > 0){
-	int elem = green_done.front();
+	elem = green_done.front();
 	green_done.pop_front();
 	green_army.push(elem);
       }
-      while(blue_done.size() > 0){
-	int elem = blue_done.front();
-	blue_done.pop_front();
-	blue_army.push(elem);
-      }      
+      
     }
-
+    
     //final print
     if(green_army.size() == blue_army.size()){//for them to be equal they must = 0
       cout << "green and blue died" << "\n";
@@ -81,8 +82,9 @@ int main(){
 	blue_army.pop();
       }
     }
-    cout << "\n";
+    if(ntest > 0)
+      cout << "\n";
   }
-  
-}
 
+  return 0;
+}
