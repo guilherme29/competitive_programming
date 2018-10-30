3ab5f76471fadba20027038e5fdd9e956fa98325
Guilherme Martins <gui.amado29@gmail.com>
2018-10-30 17:33:38 +0000

    nubian market problem resolved

--- 812c_codeforces/nubian_market.cpp
+++ 812c_codeforces/nubian_market.cpp
@@ -7,25 +7,19 @@ time complexity: O(nlog^2 n)
 
 using namespace std;
 
-int* get_price_list(int *basecost, int k, int size){
-  int *price_list = new int[size]; //tenho de criar um novo array se n n fica alocado
+long long int* get_price_list(int *basecost, int k, int size){
+  long long int *price_list = new long long int[size];
+  //tenho de criar um novo array senao nao fica alocado
   for(int i=0; i<size; i++){
-    price_list[i] = basecost[i] + (i+1)*k;
+    price_list[i] = basecost[i] + (long long int) (i+1)*k;
   }
-
-
-  printf(">>>>%d\n", k);
-  for(int i=0;i<size;i++){
-    cout << price_list[i] << " ";
-  }
-  cout << "\n";
-
+  
   sort(price_list,price_list + size);
   return price_list;
 }
 
-int get_cost(int *price_list, int k, int budget){
-  int cost = 0;
+long long int get_cost(long long int *price_list, int k, int budget){
+  long long int cost = 0;
   for(int i=0; i<k; i++){
     cost+=price_list[i];
   }
@@ -40,41 +34,32 @@ int main(){
   for(int i=0; i<ns; i++){
     scanf("%d",&basecost[i]);
   }
-
+  
 
   //binary search
-  int low = 0, high = ns, cost = 0;
-  int middle = low + (high - low)/2;
+  int low = 0, high = ns;
+  long long int cost = 0;
+  int middle = low + (high - low + 1)/2; 
+  long long int result_cost = 0; //we need a variable to store the correct cost
+  //the low variable will have the correct number of items
+  
   while(low < high){
-    middle = low + (high - low)/2;
-    int* price_list = get_price_list(basecost, middle, ns);
+    middle = low + (high - low + 1)/2; //o +1 e para arredondar para cima
+    long long int* price_list = get_price_list(basecost, middle, ns);
     cost = get_cost(price_list, middle, ns);
-
-    /*
-    cout << "--------\n";
-    for(int i=0; i<ns; i++){
-      cout << "[" << price_list[i] << "]\n";
-    }
-    cout << cost << "  " << middle << "\n";
-    cout << "--------\n";
-    */
-    
+ 
     if(cost > budget){//middle's cost is bigger than the budget
-      high = middle;
+      high = middle - 1;
     }
     else{
-      low = middle + 1;
+      result_cost = cost;
+      low = middle;
     }
   }
-
+  
   //final print
-  if(cost > budget){
-    printf("0 0\n");
-  }
-  else{
-    printf("middle:%d cost:%d\n",middle, cost);
-  }  
-
+  cout << low << " " << result_cost << "\n";
+  
   return 0;
 }
 
