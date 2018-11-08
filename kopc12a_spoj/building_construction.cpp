881ac7cc1ca4d578fddd42a88af99d7fac729689
Guilherme Martins <gui.amado29@gmail.com>
2018-11-08 19:20:58 +0000

    finished building construction problem

---
+++ kopc12a_spoj/building_construction.cpp
@@ -0,0 +1,62 @@
+#include<iostream>
+
+long long int total_cost(int* buildings, int* cost, int size, int height){
+  long long int total = 0;
+  for(int i=0; i < size; i++){
+    int k = height - buildings[i];
+    if(k < 0) { k = k*-1; } //keeping it positive
+    total += k*cost[i];
+  }
+  return total;
+}
+
+
+
+int main(){
+  int ntests; scanf("%d", &ntests);
+  while(ntests-- > 0){
+    int n; scanf("%d", &n);
+    int buildings[n];
+    for(int i=0; i<n; i++){
+      scanf("%d", &buildings[i]);
+    }
+    int cost[n];
+    for(int i=0; i<n; i++){
+      scanf("%d", &cost[i]);
+    }
+
+    //ternary search ->    log n
+    int low = 0;
+    int high = 10000;
+    int precision = 3;//minimal precision
+    
+    while(true){
+      
+      int k = high - low;
+      if(k < 0) { k = k * -1; }
+      
+      if(k < precision){
+	break;
+      }
+      
+      int mid1 = low + (high - low)/3;
+      int mid2 = high - (high - low)/3;
+      long long int cost1 = total_cost(buildings, cost, n, mid1);
+      long long int cost2 = total_cost(buildings, cost, n, mid2);
+    
+      if(cost1 >= cost2){
+	low = mid1;
+      }
+      else {
+	high = mid2;
+      }
+      
+    } 
+    
+    long long int result = (low + high) / 2;
+    printf("%lld\n", total_cost(buildings, cost, n, result));
+
+  }
+
+  return 0;
+}
