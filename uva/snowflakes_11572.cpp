4cd41cd3df93ee673be1e3901bdbd7ccf308dc05
Guilherme Martins <gui.amado29@gmail.com>
2019-02-01 09:58:01 +0000

    changed the comments so it fits the report

--- uva/snowflakes_11572.cpp
+++ uva/snowflakes_11572.cpp
@@ -1,23 +1,19 @@
 /*
 exercicio 11572_uva
-n percebi o porque de no pior caso a insercao num unordered_set ser O(n)
-acho que tem ordem linear mas posso estar enganado
 */
 
 #include<iostream>
 #include<unordered_set>
 #include<list>
-
 using namespace std;
-
 
 int main(){
   int ntest;
   cin >> ntest;
 
   while(ntest-- > 0){
-    unordered_set<int> data;
-    list<int> list1; //we need a list for a sequence like 1 2 3 2 4 5, where we have to back track to get the biggest set possible.
+    unordered_set<int> data; //set para guardar os diferentes numeros duma subsequencia
+    list<int> list1; //lista para poder fazer backtrack
 
     int n;
     cin >> n;
@@ -25,42 +21,33 @@ int main(){
     for(int i=0; i<n; i++){
       int flock;
       cin >> flock;
-      list1.push_front(flock);
-      
-      unordered_set<int>::const_iterator it = data.find(flock);
-      
-      if(it == data.end()){//if the flock isn't in yet
-	data.insert(flock);
-	if(data.size() > cur_max){
-	  cur_max = data.size();
-	}
-      }
-      else{
-	if(data.size() > cur_max){
-	  cur_max = data.size();
-	}
-	data.clear();
+      list1.push_front(flock); //adiciona-se o numero a frente da lista
 
-	//adding previous elements different from the current one
-	//no need to add the current one because we will when adding the previous ones
+      unordered_set<int>::const_iterator it = data.find(flock); //verifica-se se o numero ja esta na subsequencia corrente
 
-	
-	for(int elem : list1){
-	  it = data.find(elem);
-	  if(it == data.end()){//not in the set yet
-	    data.insert(elem);
-	  }
-	  else{
-	    break;//stop backtracking
-	  }
-	  
-	}
-	
+      if(it == data.end()){ //se o numero ainda nao esta no set
+        data.insert(flock); //insere-se
+        if(data.size() > cur_max){ //atualiza-se o cur_max
+          cur_max = data.size();
+        }
       }
-      
+      else{ //se o numero ja esta no set
+        if(data.size() > cur_max){ //atualiza-se o cur_max
+          cur_max = data.size();
+        }
+        data.clear(); //limpa-se o set
+
+        for(int elem : list1){ //faz-se backtrack e adiciona-se todos os numeros ate a repeticao
+          it = data.find(elem);
+          if(it == data.end()){
+            data.insert(elem);
+          }
+          else{
+            break;//para-se o backtracking
+          }
+        }
+      }
     }
-    cout << cur_max << "\n";
-    
+    cout << cur_max << "\n"; //print do cur_max
   }
-  
 }
